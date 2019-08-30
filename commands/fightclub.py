ef4a04fc3e8eadb898b525a0041a2eb887570453
Lemonark <alexander.olvera943@gmail.com>
2019-08-29 21:58:08 -0500

    Created new bot, setup basic params
    New bot created in Py, added basic function and implamented the use of extensions and cogs within.

---
+++ commands/fightclub.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+
+class FightClubCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        @commands.command(name='fight', description='Auto sorts players for private pvp. !fight <player1 player2 ect...')
+        @commands.guild_only()
+        async def sort_fighters(players : str):
+            return
+
+
+# The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case MembersCog.
+# When we load the cog, we use the name of the file.
+def setup(bot):
+    bot.add_cog(FightClubCog(bot))
---
+++ iota.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+
+TOKEN = "NjE1OTcyMjg2MTU5MjU3NjIx.XWiEQA.y3KCj4GkKELSpf-gZZrG7tFYzAc"
+
+
+def get_prefix(bot, message):
+
+    prefixes = ['!!', '?']
+
+    if not message.guild:
+        return '?'
+
+    return commands.when_mentioned_or(*prefixes)(bot, message)
+
+
+initial_extensions = ['commands.members',
+                      # 'Commands.fightclub',
+                      ]
+
+bot = commands.Bot(command_prefix=get_prefix, description='A Rewrite Cog Example')
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        bot.load_extension(extension)
+
+
+@bot.event
+async def on_ready():
+
+    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+
+    # Changes our bots Playing Status. type=1(streaming) for a standard game you could remove type and url.
+    await bot.change_presence(game=discord.Game(name='writing lemon jokes'))
+    print(f'Successfully logged in and booted...!')
+
+
+bot.run('TOKEN', bot=True, reconnect=True)
