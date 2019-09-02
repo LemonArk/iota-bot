946bc442281d6935d17872d9f7c4dc8b11752cb3
Lemonark <alexander.olvera943@gmail.com>
2019-09-01 19:03:52 -0500

    updated the fight command to work with getmember

--- cogs/pvp.py
+++ cogs/pvp.py
@@ -1,7 +1,7 @@
 import discord
 import random
 from discord.ext import commands
-
+from cogs.getmember import getmember
 
 class Pvp(commands.Cog):
 
@@ -9,8 +9,8 @@ class Pvp(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def fight(self, ctx, game_type='NA', *, players):
-        player_list = players.split(" ")
+    async def fight(self, ctx, *, players: discord.Role):
+        player_list = getmember(players)
         random.shuffle(player_list)
 
         temp_team_1 = player_list[:len(player_list)//2]
@@ -19,9 +19,6 @@ class Pvp(commands.Cog):
         red_team = ' '.join(temp_team_1)
         blue_team = ' '.join(temp_team_2)
 
-        """ await ctx.send('Red Team: ' + red_team)
-            await ctx.send('Blue team: ' + blue_team)
-        """
         title = [
 
             "Let's get ready to Rumble!!!",
@@ -33,9 +30,9 @@ class Pvp(commands.Cog):
         ]
 
         embed = discord.Embed(title=random.choice(title), colour=0x6262ff)
-        embed.add_field(name="Red Team", value=f'\n {red_team}')
-        embed.add_field(name="<<<<<<<<<>>>>>>>>>", value=' -  ╔════════╗  -\n-  ║   VS   ║  -\n-  ╚════════╝  -')
-        embed.add_field(name="Blue Team", value=f'\n {blue_team}')
+        embed.add_field(name="Red Team", value=f'\n {red_team}', inline=False)
+        embed.add_field(name="<<<<<<<<<>>>>>>>>>", value=' -  ╔════════╗  -\n-  ║   VS   ║  -\n-  ╚════════╝  -', inline=False)
+        embed.add_field(name="Blue Team", value=f'\n {blue_team}', inline=False)
 
         await ctx.send(content=None, embed=embed)
 
